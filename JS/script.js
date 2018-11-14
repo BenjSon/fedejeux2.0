8f6b7d550c7c54eeff74b439bed52e2546a36534
thomyleterrien <44745224+thomyleterrien@users.noreply.github.com>
2018-11-14 19:12:33 +0100

    HTML final
    Avec ça le CSS sera plus facile

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,18 @@
 <script>
-
+    new Vue({
+        el:'#salles',
+        data: {
+            
+        },
+        methods: {
+            
+        },
+        computed: {
+            
+        },
+        watch: {
+            
+        }
+                
+    });
 </script>
