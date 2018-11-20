a90dccf6c75dae3c78932d705409f0b5d8d9011a
thomyleterrien <44745224+thomyleterrien@users.noreply.github.com>
2018-11-20 09:27:28 +0100

    Reprise de détails

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,1 @@
-<script>
-    new Vue({
-        el:'#salles',
-        data: {
-            
-        },
-        methods: {
-            
-        },
-        computed: {
-            
-        },
-        watch: {
-            
-        }
-                
-    });
-</script>
+
