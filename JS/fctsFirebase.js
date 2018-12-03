56052439c4038cfc64cda404f0ce433737988ad4
Benjamin <sonneville.b@hotmail.fr>
2018-12-03 12:38:06 +0100

    lien firebase + fct reset tozero

--- JS/fctsFirebase.js
+++ JS/fctsFirebase.js
@@ -1,3 +1,4 @@
+
 getHistory = function(salle){
 	
 	return historique;
@@ -7,3 +8,18 @@ getLastofH = function(historique){
 	var taille = historique.length;
 	return historique[taille-1];
 };
+
+
+ResetToZero = function(nombreSalles){
+	// 3 salles étant la base initiale du projet 
+	if((nombreSalles===undefined)){
+		var nombreSalles = 3;
+	}
+	for(var i = 1; i<=nombreSalles;i++){
+		db.collection("HistoriqueSalles").doc("décompte1").collection("Salle"+i).add({
+			"nbr": 0,
+			"temps": 0
+		});
+	}
+	
+}
