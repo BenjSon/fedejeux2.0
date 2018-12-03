ca981fd64ba3855cec01662067a707a22510366e
Adrien Morget <claessen.lena@gmail.com>
2018-12-03 23:24:30 +0100

    Update fct firebase

--- JS/fctsFirebase.js
+++ JS/fctsFirebase.js
@@ -1,36 +1,5 @@
-var DATA = new Vue({
-	el: "#data",
-	data: {
+var dataAffluence = firebase.database().ref('Affluence');
+dataAffluence.on('value',function(snapshot){
+	console.log(snapshot.key)
+})
 
-	},
-	methods:{
-		toAccueil:function(){
-			CONNEXION.showConnexion = false,
-			ACCUEIL.showAccueil = true,
-			ADMIN.showAdmin = false,
-			SALLES.showSalles = false,
-			HEADER.showHeader = true
-		},
-		toAdmin:function(){
-			CONNEXION.showConnexion = false,
-			ACCUEIL.showAccueil = false,
-			ADMIN.showAdmin = true,
-			SALLES.showSalles = false,
-			HEADER.showHeader = true
-		},
-		toConnexion:function(){
-			CONNEXION.showConnexion = true,
-			ACCUEIL.showAccueil = false,
-			ADMIN.showAdmin = false,
-			SALLES.showSalles = false,
-			HEADER.showHeader = true
-		},
-		toSalles:function(){
-			CONNEXION.showConnexion = false,
-			ACCUEIL.showAccueil = false,
-			ADMIN.showAdmin = false,
-			SALLES.showSalles = true,
-			HEADER.showHeader = true
-		}
-	}
-});
