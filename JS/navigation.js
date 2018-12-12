571e4825d22bf34d8391cb6684bf0e59a7d8c30d
Amélie Gombeaud <amelie.gombeaud@live.fr>
2018-12-12 11:33:03 +0100

    modif

--- JS/navigation.js
+++ JS/navigation.js
@@ -9,28 +9,32 @@ var NAV = new Vue({
 			ACCUEIL.showAccueil = true,
 			ADMIN.showAdmin = false,
 			SALLES.showSalles = false,
-			HEADER.showHeader = true
+			HEADER.showHeader = true,
+			FOOTER.showFooter = true
 		},
 		toAdmin:function(){
 			CONNEXION.showConnexion = false,
 			ACCUEIL.showAccueil = false,
 			ADMIN.showAdmin = true,
 			SALLES.showSalles = false,
-			HEADER.showHeader = true
+			HEADER.showHeader = true,
+			FOOTER.showFooter = true
 		},
 		toConnexion:function(){
 			CONNEXION.showConnexion = true,
 			ACCUEIL.showAccueil = false,
 			ADMIN.showAdmin = false,
 			SALLES.showSalles = false,
-			HEADER.showHeader = true
+			HEADER.showHeader = false
+			FOOTER.showFooter = false
 		},
 		toSalles:function(){
 			CONNEXION.showConnexion = false,
 			ACCUEIL.showAccueil = false,
 			ADMIN.showAdmin = false,
 			SALLES.showSalles = true,
-			HEADER.showHeader = true
+			HEADER.showHeader = true,
+			FOOTER.showFooter = true
 		}
 	}
 });
