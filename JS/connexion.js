79490b7ef3de38def8945bf5a1ddd7c92a367f9a
thomasDeCoucy <45848195+thomasDeCoucy@users.noreply.github.com>
2018-12-19 11:30:15 +0100

    update

--- JS/connexion.js
+++ JS/connexion.js
@@ -1,7 +1,37 @@
-var CONNEXION = new Vue({
+ var CONNEXION = new Vue({
 	el: "#connexion",
 	data: {
-		showConnexion: true
+		showConnexion: true,
+        user: {identifiant: "", mdp: ""}
 	},
-	methods:{}
+	methods:{
+        // fonction qui redirige vers l'accueil si l'utilisateur existe.
+        seConnecter:function(){
+            console.log(this.user);
+            // on vérifie si quelqu'un est déjà connecté
+            if (firebase.auth().currentUser) {
+                //si quelqu'un est connecté on le déconnecte
+                firebase.auth().signOut();
+            }
+            var identifiant = this.user.identifiant;
+            var mdp = this.user.mdp;
+            //on se connecte
+            firebase.auth().signInWithEmailAndPassword(identifiant, mdp)
+                            .catch(function(error) {
+                                //on gère les erreurs ici
+                                var errorCode = error.code;
+                                var errorMessage = error.message;
+                                //on affiche les erreurs possibles
+                                if (errorCode === 'auth/wrong-password') {
+                                    alert('Mauvais mot de passe');
+                                } else {
+                                    alert(errorMessage);
+                                }
+                                console.log(error);
+                            });
+            if (firebase.auth().currentUser){
+                return NAV.toAccueil();
+            }                
+        }
+    }
 });
